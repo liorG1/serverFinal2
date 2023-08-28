e67c3b6d833b318795abb6ac08e95fc3bc084c67
liorG1 <106651304+liorG1@users.noreply.github.com>
2023-08-28 19:38:37 +0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 
 let mongoose=require('mongoose')
 
-let url='mongodb+srv://liorgetahun4:liorgetahun4@cluster0.dppxhbx.mongodb.net/finalProject_1'
+let url='mongodb+srv://liorgetahun4:liorDatabase100@cluster0.dppxhbx.mongodb.net/finalProject_1'
 
 let connection=()=> mongoose.connect(url)
 
